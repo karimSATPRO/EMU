d573c6ce091074b9a6c41a4847cdc1bec3ac1514
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2023-09-01 00:07:04 +0100

    Update supcam-oscam.sh

--- supcam-oscam.sh
+++ supcam-oscam.sh
@@ -1,1 +1,19 @@
-https://github.com/karimSATPRO/EMU/raw/main/enigma2-plugin-softcams-supcam-oscam_11.725-emu-r798_all.ipk
+
+echo "##############################################"
+écho "#############################################" 
+echo "##############################################"
+echo "############ SupCam-Oscam_11725 IPK #################"
+
+wget -O /tmp/enigma2-plugin-softcams-supcam-oscam_11.725-emu-r798_all.ipk "https://github.com/karimSATPRO/EMU/raw/main/enigma2-plugin-softcams-supcam-oscam_11.725-emu-r798_all.ipk"
+
+opkg install --force-overwrite /tmp/*.ipk
+
+echo ""
+cd ..
+sync
+echo "############ Installation Successful  ########"
+echo "############ Restart Enigma2 GUI... #################" 
+init 4
+sleep 2
+init 3
+exit 0
